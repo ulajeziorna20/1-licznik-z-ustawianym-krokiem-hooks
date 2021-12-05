cdcd4481a15fa0256904063a1879ac959294157f
Ula Jeziorna <jeziornaula@wp.pl>
2021-12-05 15:35:10 +0100

    Formatowanie kodu.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Counter from './components/Counter';
+import logo from "./logo.svg"
+import "./App.css"
+import Counter from "./components/Counter"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <Counter initValue={108} />
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
